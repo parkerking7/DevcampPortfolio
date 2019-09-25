4c6253aeaae498a2a8e6571baee1bd3f8681f44b
Parker King <parker.king0807@gmail.com>
2019-09-25 11:47:42 -0600

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,3 +44,11 @@ end
 	)
 end
 puts "9 Portfolio Items created"
+
+3.times do |technology|
+	Portfolio.last.technologies.create!(
+		name: "Technology #{technology}"
+	)
+end
+
+puts "3 Technoligies created"
